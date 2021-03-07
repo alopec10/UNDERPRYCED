70420e57f2b2436d2c8deebd57f4bff5757ff8f3
Alvaro Lb <alopec10@estudiantes.unileon.es>
2021-03-07 13:47:54 +0100

    Project set up (Spring Boot and Vue)

---
+++ Backend/src/main/java/com/inso2/inso2/Inso2Application.java
@@ -0,0 +1,13 @@
+package com.inso2.inso2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inso2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Inso2Application.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/inso2/inso2/Inso2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inso2.inso2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Inso2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
