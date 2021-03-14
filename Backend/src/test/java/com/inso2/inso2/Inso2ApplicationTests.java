1c82028b9bfeb194b21ad201604300883b309627
Álvaro González Jiménez <agonzj02@estudiantes.unileon.es>
2021-03-14 13:01:59 +0100

    Check tests are passed
    Simple test added to check that the tests are correctly passed in the workflow

--- Backend/src/test/java/com/inso2/inso2/Inso2ApplicationTests.java
+++ Backend/src/test/java/com/inso2/inso2/Inso2ApplicationTests.java
@@ -1,13 +1,32 @@
 package com.inso2.inso2;
 
+import com.inso2.inso2.domain.entity.Category;
+import com.inso2.inso2.domain.persistence.CategoryRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Inso2ApplicationTests {
 
+	@Autowired
+	CategoryRepository categoryRepository;
+
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	public void createCategory() throws Exception {
+		Category c  = new Category("Testing");
+		categoryRepository.save(c);
+		assertEquals(2, categoryRepository.count());
+	}
+
+
+
+
 
 }
