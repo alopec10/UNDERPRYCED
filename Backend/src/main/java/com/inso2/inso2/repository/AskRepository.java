d605ae235fe81661f0d2b1dc6f67b3d310da821b
Álvaro González Jiménez <agonzj02@estudiantes.unileon.es>
2021-04-08 18:00:46 +0200

    Repository Type changed from CRUD to JPA

--- Backend/src/main/java/com/inso2/inso2/repository/AskRepository.java
+++ Backend/src/main/java/com/inso2/inso2/repository/AskRepository.java
@@ -1,7 +1,8 @@
 package com.inso2.inso2.repository;
 
 import com.inso2.inso2.model.Ask;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AskRepository extends CrudRepository<Ask, Long> {
+public interface AskRepository extends JpaRepository<Ask, Long> {
 }
