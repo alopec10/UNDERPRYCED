7837db5a9b23887b36f70ee29a655ad91f6871c8
Álvaro González Jiménez <agonzj02@estudiantes.unileon.es>
2021-04-11 12:56:51 +0200

    Get products by ref implemented

--- Backend/src/main/java/com/inso2/inso2/controller/ProductController.java
+++ Backend/src/main/java/com/inso2/inso2/controller/ProductController.java
@@ -1,14 +1,12 @@
 package com.inso2.inso2.controller;
 
-import com.inso2.inso2.dto.authentication.AuthenticationResponse;
-import com.inso2.inso2.dto.product.ProductSpecificationRequest;
-import com.inso2.inso2.dto.product.ProductSpecificationResponse;
-import com.inso2.inso2.dto.product.SpecificationDTO;
-import com.inso2.inso2.dto.register.RegisterRequest;
+import com.inso2.inso2.dto.product.byRef.ProductByRefRequest;
+import com.inso2.inso2.dto.product.specification.ProductSpecificationRequest;
+import com.inso2.inso2.dto.product.specification.ProductSpecificationResponse;
+import com.inso2.inso2.dto.product.specification.SpecificationDTO;
 import com.inso2.inso2.model.Product;
 import com.inso2.inso2.repository.ProductRepository;
 import com.inso2.inso2.repository.specification.SearchCriteria;
-import com.inso2.inso2.repository.specification.SearchOperation;
 import com.inso2.inso2.repository.specification.SearchOperationUtils;
 import com.inso2.inso2.repository.specification.product.ProductSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +33,16 @@ public class ProductController {
             specification.add(new SearchCriteria(sp.getKey(), sp.getValue(), SearchOperationUtils.convert(sp.getOperator())));
         }
         List<Product> products = productRepository.findAll(specification);
+        ArrayList<ProductSpecificationResponse> resp = new ArrayList<ProductSpecificationResponse>();
+        for(Product p: products){
+            resp.add(new ProductSpecificationResponse().build(p));
+        }
+        return ResponseEntity.ok(resp);
+    }
+
+    @RequestMapping(value = "/ref", method = RequestMethod.POST)
+    public ResponseEntity<?> getProductsByRef(@RequestBody ProductByRefRequest req){
+        List<Product> products = productRepository.findByRefIn(req.getRefs());
         ArrayList<ProductSpecificationResponse> resp = new ArrayList<ProductSpecificationResponse>();
         for(Product p: products){
             resp.add(new ProductSpecificationResponse().build(p));
--- Backend/src/main/java/com/inso2/inso2/repository/ProductRepository.java
+++ Backend/src/main/java/com/inso2/inso2/repository/ProductRepository.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+    List<Product> findByRefIn(Collection<String> refs);
 }
