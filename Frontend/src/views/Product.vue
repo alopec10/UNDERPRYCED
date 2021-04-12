4751111b0e96262ab9a8ebdb45b5a4ea4386866b
Alvaro Lb <alopec10@estudiantes.unileon.es>
2021-04-12 23:22:12 +0200

    Aesthetic of divs improved

--- Frontend/src/views/Product.vue
+++ Frontend/src/views/Product.vue
@@ -3,22 +3,31 @@
     <h1 class="text-6xl my-4">
       {{ title }}
     </h1>
-    <div id="LastSale">
-      Last Sale: 430$
+    <div class="inline-flex align-center my-6">
+      <div id="LastSale" class="w-32 h-20 sm:w-48 sm:h-24 bg-purple-300 rounded-lg mx-4 flex justify-center items-center">
+        <h1 class="text-white text-md sm:text-2xl ">
+          LAST SALE: 430$
+        </h1>
+      </div>
+      <div id="LowestAsk" class="w-32 h-20 sm:w-48 sm:h-24 bg-purple-700 rounded-lg m-auto flex justify-center items-center">
+          <h1 class="text-white text-md sm:text-2xl m-auto">
+            LOWEST ASK: 444$
+          </h1>
+      </div>
+      <div id="HighestBid" class="w-32 h-20 sm:w-48 sm:h-24 bg-purple-500 rounded-lg mx-4 flex justify-center items-center">
+        <h1 class="text-white text-md sm:text-2xl ">
+          HIGHEST BID: 420$
+        </h1>
+      </div>
     </div>
-    <div id="LowestAsk">
-      Lowest Ask: 444$
-    </div>
-    <div id="HighestBid">
-      Highest Bid: 420$
-    </div>
+
     <img :src="url" class="mx-auto my-6 p-5 border-2 rounded-lg shadow-xl border-purple-300"/>
 
     <div>
       <h1>Retail price {{ retail_price }}</h1>
       <h1>Release date {{ rel_date }}</h1>
     </div>
-<button @click="update">a</button>
+    <button @click="update">a</button>
   </div>
 
 </template>
