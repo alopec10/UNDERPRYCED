33db5ac580630d042f7f1131c1ff02fade7126a1
Álvaro González Jiménez <agonzj02@estudiantes.unileon.es>
2021-04-21 18:29:54 +0200

    Authenticate now returns the role of the user

--- Backend/src/main/java/com/inso2/inso2/dto/authentication/AuthenticationResponse.java
+++ Backend/src/main/java/com/inso2/inso2/dto/authentication/AuthenticationResponse.java
@@ -1,16 +1,24 @@
 package com.inso2.inso2.dto.authentication;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import java.io.Serializable;
 
 public class AuthenticationResponse implements Serializable {
 
     private final String jwt;
+    private final String role;
 
-    public AuthenticationResponse(String jwt) {
+    public AuthenticationResponse(String jwt, GrantedAuthority role) {
         this.jwt = jwt;
+        this.role = role.toString();
     }
 
     public String getJwt() {
         return jwt;
+    }
+
+    public String getRole() {
+        return role;
     }
 }
