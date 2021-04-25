8d9c9e8ceb5673006fc8bcd90b54a74186d9d8cc
Álvaro González Jiménez <agonzj02@estudiantes.unileon.es>
2021-04-25 22:12:45 +0200

    Ship and Arrival date modified when an order is approved

--- Backend/src/main/java/com/inso2/inso2/service/order/ApproveOrderService.java
+++ Backend/src/main/java/com/inso2/inso2/service/order/ApproveOrderService.java
@@ -9,6 +9,9 @@ import com.inso2.inso2.repository.OrderRepository;
 import com.inso2.inso2.repository.ShipmentRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -31,6 +34,8 @@ public class ApproveOrderService {
         warehouseShipment.setStatus(ShipmentStatus.COMPLETED);
         this.shipmentRepository.saveAndFlush(warehouseShipment);
         homeShipment.setSent(true);
+        homeShipment.setShipDate(new Date());
+        homeShipment.setArrivalDate(this.calculateDatePeriod(new Date(),3));
         homeShipment.setApproved(true);
         homeShipment.setStatus(ShipmentStatus.ON_WAY);
         this.shipmentRepository.saveAndFlush(homeShipment);
@@ -54,5 +59,34 @@ public class ApproveOrderService {
             }
         }
         return null;
+    }
+
+    private long getAllDays(int dayOfWeek, long businessDays) {
+        long result = 0;
+        if (businessDays != 0) {
+            boolean isStartOnWorkday = dayOfWeek < 6;
+            long absBusinessDays = Math.abs(businessDays);
+
+            if (isStartOnWorkday) {
+                // if negative businessDays: count backwards by shifting weekday
+                int shiftedWorkday = businessDays > 0 ? dayOfWeek : 6 - dayOfWeek;
+                result = absBusinessDays + (absBusinessDays + shiftedWorkday - 1) / 5 * 2;
+            } else { // start on weekend
+                // if negative businessDays: count backwards by shifting weekday
+                int shiftedWeekend = businessDays > 0 ? dayOfWeek : 13 - dayOfWeek;
+                result = absBusinessDays + (absBusinessDays - 1) / 5 * 2 + (7 - shiftedWeekend);
+            }
+        }
+        return result;
+    }
+
+    private Date calculateDatePeriod(Date date, long businessDays){
+        LocalDate startDate = date.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+        LocalDate endDate = startDate.plusDays(getAllDays(startDate.getDayOfWeek().getValue(), businessDays));
+        return Date.from(endDate.atStartOfDay()
+                .atZone(ZoneId.systemDefault())
+                .toInstant());
     }
 }
