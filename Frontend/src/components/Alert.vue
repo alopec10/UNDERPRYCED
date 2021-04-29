205ffba49bfc80e2b0e4a742678198194f12a3fe
Alvaro Lb <alopec10@estudiantes.unileon.es>
2021-04-29 22:33:23 +0200

    Alert waiting for css

--- Frontend/src/components/Alert.vue
+++ Frontend/src/components/Alert.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="border-2 border-purple-500 h-20 rounded-lg width mx-auto">
-    <h1>a</h1>
+  <div class="border-2 border-purple-500 h-20 rounded-lg width mx-auto flex">
+    <div class="mt-3">
+      <h1 class="text-xl">{{alert.title}}</h1>
+      <h1 class="text-md mt-3">{{alert.message}}</h1>
+    </div>
+    <div class="justify-center items-center text-purple-500">
+      <i class="fas fa-check block text-xl mt-6 mr-1 cursor-pointer"></i>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: "Alert"
+  name: "Alert",
+  props: {
+    alert: {
+      type: Object,
+      required: true
+    }
+  }
+
 }
 </script>
 
