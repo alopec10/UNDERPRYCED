58d3a52a6fb277997213148b3fed425f0e8217b3
Álvaro González Jiménez <agonzj02@estudiantes.unileon.es>
2021-05-19 19:22:11 +0200

    New modal implemented

--- Frontend/src/views/ConfirmBuy.vue
+++ Frontend/src/views/ConfirmBuy.vue
@@ -76,18 +76,20 @@
         </div>
         <modal ref="modalName">
           <template v-slot:header>
-            <h1>Modal title</h1>
+            <h1 class="text-4xl font-semibold mb-10" style="font-family:'Quantico'">COMPRA CONFIRMADA</h1>
           </template>
 
           <template v-slot:body>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed velit dignissim sodales ut eu sem integer vitae. Id aliquet lectus proin nibh nisl condimentum. Fringilla urna porttitor rhoncus dolor purus. Nam aliquam sem et tortor. Nisl vel pretium lectus quam id. Cras pulvinar mattis nunc sed. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Tristique magna sit amet purus. Fermentum et sollicitudin ac orci phasellus egestas tellus. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Felis eget nunc lobortis mattis aliquam faucibus. Tincidunt eget nullam non nisi est sit amet facilisis. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Eget nunc scelerisque viverra mauris in aliquam sem fringilla ut. Nec nam aliquam sem et tortor consequat id. Commodo nulla facilisi nullam vehicula ipsum a. Elementum tempus egestas sed sed. Faucibus purus in massa tempor nec feugiat nisl pretium fusce.</p>
-            <p>Arcu cursus vitae congue mauris rhoncus aenean. Tempor id eu nisl nunc mi. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Ut faucibus pulvinar elementum integer enim. Odio facilisis mauris sit amet massa vitae tortor condimentum lacinia. Eu non diam phasellus vestibulum lorem sed risus. Porttitor rhoncus dolor purus non enim praesent. Sit amet mauris commodo quis imperdiet. Lobortis feugiat vivamus at augue eget. Nibh tellus molestie nunc non blandit. Tellus mauris a diam maecenas sed enim ut. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Mattis aliquam faucibus purus in massa.</p>
+            <p>Tan pronto como recibamos el producto y podamos comprobar su veracidad procederemos a enviar el producto a su domicilio</p>
+            <p class="mt-3">Si tiene cualquier duda o pregunta no dude en contactar con nosotros en la siguiente dirección de correo electrónico: customers@underpryced.com</p>
+            <p class="mt-3">¡Gracias por confiar en nosotros!</p>
           </template>
 
           <template v-slot:footer>
             <div>
-              <button @click="$refs.modalName.closeModal()">Cancel</button>
-              <button @click="$refs.modalName.closeModal()">Save</button>
+              <button type="button" style="font-family:'Quantico'"
+                      class="bg-purple-500 text-xl p-3 px-5 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105"
+                  @click="$refs.modalName.closeModal(); $router.push('/')">Cerrar</button>
             </div>
           </template>
         </modal>
@@ -200,7 +202,6 @@ export default {
       console.log(dat)
       axios({url: 'http://localhost:8888/order/createBuy', data: dat, method: 'POST'})
           .then(resp => {
-            alert("Venta creada correctamente")
             this.$refs.modalName.openModal()
           })
           .catch(err => {
